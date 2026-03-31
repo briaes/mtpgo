1204ae244be7fb99b1b2d5abe69c5fa292b69de7
briaes <106591422+briaes@users.noreply.github.com>
2026-04-01 04:42:33 +0800

    Update const.go

--- const.go
+++ const.go
@@ -3,8 +3,6 @@ package main
 import "encoding/hex"
 
 const (
-	TGDatacenterPort = 443
-
 	SkipLen      = 8
 	PrekeyLen    = 32
 	KeyLen       = 32
@@ -26,16 +24,6 @@ var (
 	ProtoTagSecure       = []byte{0xdd, 0xdd, 0xdd, 0xdd}
 	PaddingFiller        = []byte{0x04, 0x00, 0x00, 0x00}
 )
-
-var TGDatacentersV4 = []string{
-	"149.154.175.50", "149.154.167.51", "149.154.175.100",
-	"149.154.167.91", "149.154.171.5",
-}
-
-var TGDatacentersV6 = []string{
-	"2001:b28:f23d:f001::a", "2001:67c:04e8:f002::a", "2001:b28:f23d:f003::a",
-	"2001:67c:04e8:f004::a", "2001:b28:f23f:f005::a",
-}
 
 // 运行时会更新
 var TGMiddleProxiesV4 = map[int][][2]interface{}{
